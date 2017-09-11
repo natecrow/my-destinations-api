40461a5f0d88e34a744688be5f7b18bc10212412
natecrow <nate.crow76@gmail.com>
2017-09-11 00:24:58 -0400

    Started API with endpoint that takes in a song name

--- src/main/java/com/playlist/api/playlistapi/PlaylistApiApplication.java
+++ src/main/java/com/playlist/api/playlistapi/PlaylistApiApplication.java
@@ -2,11 +2,23 @@ package com.playlist.api.playlistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PlaylistApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaylistApiApplication.class, args);
 	}
+	
+	@PostMapping("/playlist/{song}")
+	public String addSongToPlaylist(@PathVariable String song) {
+		String payload = song + " posted...";
+		return payload;
+	}
+	
 }
