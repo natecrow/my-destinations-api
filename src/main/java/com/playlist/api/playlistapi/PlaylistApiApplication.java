00b0efe480c6355046a319a1e85b6c9a59ee5660
natecrow <nate.crow76@gmail.com>
2017-09-10 20:48:30 -0400

    Initial Java Spring set up from Spring Initializer

---
+++ src/main/java/com/playlist/api/playlistapi/PlaylistApiApplication.java
@@ -0,0 +1,12 @@
+package com.playlist.api.playlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistApiApplication.class, args);
+	}
+}
